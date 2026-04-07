2b55a0d9084c4b29eaa8806ed74fe8801f318aa2
yunhacandy <yunhacandy@gmail.com>
2026-04-07 17:46:30 +0900

    feat: #5 기본 화면 레이아웃 구현

---
+++ frontend/src/components/layout/LayoutWrapper.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <main>
+            <slot />
+        </main>
+        <BottomNav />
+    </div>
+</template>
+
+<script setup>
+import BottomNav from '@/components/layout/BottomNav.vue'
+</script>
