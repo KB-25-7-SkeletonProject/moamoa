dbf3602561ca8eecacc8aa77b9503d705637c27e
yunhacandy <yunhacandy@gmail.com>
2026-04-07 16:57:10 +0900

    feat: #5 기본 헤더 구현

---
+++ frontend/src/components/layout/AppHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class="header">
+        <div class="text-group">
+            <div class="title">{{ title }}</div>
+            <div class="desc">{{ desc }}</div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    desc: String
+})
+</script>
+
+<style scoped>
+.header {
+    background: var(--primary);
+    padding: 16px;
+
+    display: flex;
+    align-items: center;
+
+    position: sticky;
+    top: 0;
+    z-index: 999;
+}
+
+.text-group {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+.title {
+    font-size: 18px;
+    font-weight: 700;
+    color: var(--text);
+}
+
+.desc {
+    font-size: 12px;
+    color: var(--text-secondary);
+}
+</style>
