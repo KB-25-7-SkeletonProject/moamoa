bbadc5f1d1df3e6c8ef506a2c1593740f338a3cf
yunhacandy <yunhacandy@gmail.com>
2026-04-08 17:38:41 +0900

    feat: #11 LayoutWrapper 공통 헤더 props 추가

--- frontend/src/components/layout/LayoutWrapper.vue
+++ frontend/src/components/layout/LayoutWrapper.vue
@@ -11,4 +11,19 @@
 <script setup>
 import AppHeader from '@/components/layout/AppHeader.vue'
 import BottomNav from '@/components/layout/BottomNav.vue'
+
+defineProps({
+    title: String,
+    desc: String
+})
 </script>
+
+<style scoped>
+.layout {
+    min-height: 100vh;
+}
+
+main {
+    padding-bottom: 60px;
+}
+</style>
